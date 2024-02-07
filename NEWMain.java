6a2fc24626e796c3901ac73f3f3699bce101a923
Dari <icatobe76@gmail.com>
2024-02-07 23:29:33 +0300

    Third commit

--- NEWMain.java
+++ NEWMain.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class NEWMain {
     public static void main(String[] args) {
         int x = 9;
+        Scanner console = new Scanner(System.in());
+        int y = console.nextInt();
+        System.out.print (x+y);
     }
 }
