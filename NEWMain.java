8c42fb654069055787642634b9ffe2a22dd64627
Natalia <natas9903@yandex.ru>
2024-02-11 19:57:06 +0300

    Natalia test commit

--- NEWMain.java
+++ NEWMain.java
@@ -26,3 +26,13 @@ public class NEWMain {
         System.out.print (x+y);
     }
 }
+
+public class NEWMain {
+    public static void main(String[] args) {
+        int x = 9;
+        Scanner console = new Scanner(System.in());
+        int y = console.nextInt();
+        System.out.print (x+y+1);
+    }
+}
+
