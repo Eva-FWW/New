32c7788eae6ed9d55dc6ede9acf409ae2b4bf4cb
Roman Pecherskikh <pwpec95@gmail.com>
2024-02-11 20:14:12 +0300

    Test Commit

--- NEWMain.java
+++ NEWMain.java
@@ -36,3 +36,12 @@ public class NEWMain {
     }
 }
 
+public class NEWMain {
+    public static void main(String[] args) {
+        int x = 9;
+        Scanner console = new Scanner(System.in());
+        int y = console.nextInt();
+        System.out.print (x+y+1);
+    }
+}
+
