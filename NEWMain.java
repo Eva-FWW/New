31c758ea18ad34d97ebc89bb09cc3823ebe3b9c5
Roman Pecherskikh <pwpec95@gmail.com>
2024-02-19 19:35:25 +0300

    19.02.2024

--- NEWMain.java
+++ NEWMain.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class NEWMain {
     public static void main(String[] args) {
-        int x = 9;
-        Scanner console = new Scanner(System.in());
-        int y = console.nextInt();
-        System.out.print (x+y);
-        int u = console.nextInt();
+        System.out.println("Privet");
     }
 }
